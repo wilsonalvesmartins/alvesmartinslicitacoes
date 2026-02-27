790d526b4d743d92c5d240c198b3505b4bd9125c
wilsonalvesmartins <wilsonalves.jur@gmail.com>
2026-02-27 17:15:44 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Garante que o build vai para a pasta correta que o server.js espera
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
